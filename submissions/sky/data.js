f8e163133ead2fc6578d29aad529fa103be538e4
Sigrunn Sky <sigrunn.sky@colorado.edu>
2017-02-16 16:25:44 -0700

    More comments!

--- submissions/sky/data.js
+++ submissions/sky/data.js
@@ -3,13 +3,15 @@ var w = 700;// Width of our visualization
 var h= 500;// Height of our visualization
 var xOffset = 100;// Space for x-axis labels
 var yOffset = 100;// Space for y-axis labels
-var margin = 0;// Margin around visualization 
+var margin = 0;// Margin around visualization. I set this to zero because it was causing a gap in my axis. I instead changed the y offset to create space between my graphs. 
 var vals = ['x', 'y']; //List of data attributes
 var xVal = vals[0]; // Value to plot on x-axis
 var yVal = vals[1];// Value to plot on y-axis
 var hForReal = h - yOffset- margin;
+
 // Next, we will load in our CSV of data
-d3.csv('data/anscombe_I.csv', function (csvData){ return checkDataset(csvData)});
+//PART 1
+d3.csv('data/anscombe_I.csv', function (csvData){ return checkDataset(csvData)}); //Got help at office hours. Calling our callback function.
 d3.csv('data/anscombe_II.csv', function (csvData){ return checkDataset(csvData)});
 d3.csv('data/anscombe_III.csv', function (csvData){ return checkDataset(csvData)});
 d3.csv('data/anscombe_IV.csv', function (csvData){ return checkDataset(csvData)});
@@ -22,13 +24,13 @@ d3.csv('data/anscombe_II.csv', function(csvData){
 
 	//Define scales tat concert from the data domain to screen coordinates
 	var xScale=d3.scale.linear()
-			.domain([0, d3.max(data, function(d){
+			.domain([0, d3.max(data, function(d){ //changed the min to 0
 				return parseFloat(d[xVal]);
 			})+1])
 			.range([xOffset + margin, w-20]);
 
 	var yScale=d3.scale.linear()
-			.domain([0, d3.max(data, function(d){
+			.domain([0, d3.max(data, function(d){ //changed the min to 0
 				return parseFloat(d[xVal]);
 			})+1])
 			.range([h- yOffset - margin, margin]);
@@ -41,7 +43,7 @@ d3.csv('data/anscombe_II.csv', function(csvData){
 									.attr("width", w)
 									.attr("height", h);
 
-	// Build axes! (These are kind of annoying, actually...)
+	// Build axes! 
 	//x-axis
 	var xAxis = d3.svg.axis()
 					  .scale(xScale)
@@ -87,13 +89,13 @@ d3.csv('data/anscombe_II.csv', function(csvData){
 					.data(data);
 
 		// Create new elements if needed
-	bar.enter().append("svg:rect"); //enter is how to do loops in d3.. keeps going until we say exit.
+	bar.enter().append("svg:rect"); //rectangles to make our bars.
 		// Update our selection
 	bar.attr("x",  function(d) {return xScale(d[xVal]);}) 
-		.attr("y", function(d) { return yScale(d[xVal]);})
-		.attr("height", function(d) {return hForReal - yScale(d[xVal]);})
-		.attr("width", 20)
-		.attr("fill", "pink");
+		.attr("y", function(d) { return yScale(d[xVal]);}) //xVal because you wanted x value scaled to the y axis.
+		.attr("height", function(d) {return hForReal - yScale(d[xVal]);}) //hForReal is defined up top. To help us put our graph in the correct spot!
+		.attr("width", 20) //width of our rectangle
+		.attr("fill", "pink"); //fill pink
     
     // Now, let's select all of our points and change their color
 });
@@ -176,19 +178,10 @@ d3.csv('data/anscombe_II.csv', function(csvData){
 
 		// Update our selection
 	point.attr("class","point")
-		.attr("cx", function(d) {return xScale(d[xVal]);}) //we never exited from point, so its gonna keep going. 
+		.attr("cx", function(d) {return xScale(d[xVal]);}) //cx (position of our point) corresponts to our x-value
 		.attr("cy", function(d) {return yScale(d[yVal]);})
 		.attr("r",10) //radius
-		.attr("fill", "purple");
-		 
-
-    point.append("svg:title")
-         .text(function(d) {return d['x'];});
-
-	// var title = d3.select("#scatterLabel").append("text")
- //   				   .attr("class", "label")
- // 			       .text("needs work");
-
+		.attr("fill", "purple"); //color
 });
  
 
@@ -276,9 +269,10 @@ d3.csv('data/anscombe_II.csv', function(csvData){
 		.attr("cy", function(d) {return yScale(d[yVal]);})
 		.attr("r",10) //radius
 		.attr("fill", "red")
-		.on("mouseover", function(d) {d3.select(this).attr("r", 15).attr("fill", "#660000");})
-		.on("mouseout", function(d) {d3.select(this).attr("r", 10).style("fill", "red");})
+		.on("mouseover", function(d) {d3.select(this).attr("r", 15).attr("fill", "#660000");}) //mouseover changes attribute "r", radius and changes fill to a darker red.
+		.on("mouseout", function(d) {d3.select(this).attr("r", 10).style("fill", "red");}) // mouseout changes back to original size and color.
 		.on("click", function(d) {d3.select("#scatterLabel").text("( "+d[xVal]+", "+ d[yVal]+" )").style("font-size", "30px"); });
+		// when we click our points, we select the #scatterLabel and update the text to the current datapoint's x and y value. Also, I added fontsize because it kept changing it to a smaller font size. funky. 
 	
 });
 
@@ -383,12 +377,12 @@ d3.csv('data/anscombe_I.csv', function(csvData){
 d3.csv('data/anscombe_II.csv', function(csvData){
 	var data = csvData;
 
-	//Define scales tat concert from the data domain to screen coordinates
+	//Define scales that convert from the data domain to screen coordinates
 	var xScale=d3.scale.linear()
 			.domain([0, d3.max(data, function(d){
 				return parseFloat(d[xVal]);
 			})+1])
-			.range([xOffset + margin, w-20]);
+			.range([xOffset + margin, w-20]); // because the margin was messing stuff up, I changed to w- 20 instead of w- margin. And set the margin to 0.
 
 	var yScale=d3.scale.linear()
 			.domain([0, d3.max(data, function(d){
@@ -446,14 +440,14 @@ d3.csv('data/anscombe_II.csv', function(csvData){
 			        .style("font-family", "sans-serif")
 					.text(yVal);
 
-	var titles = d3.select("#scatterLabel2").append("text") //link to #scatter.. in HTML Doc
+	var titles = d3.select("#scatterLabel2").append("text") //link to #scatterLabel2.. in HTML Doc
    				    .attr("text-anchor", "middle") //try to pin it in middle.. couldnt figure out why this isnt working
    				    .attr("class", "titles")
      			    .attr("x", w/2) //dont know why this is not working
-			        .attr("y", 0)
+			        .attr("y", 0) 
 			        .style("font-size", "30px")
 			        .style("font-weight", "bold")
-			        .style("font-family", "sans-serif")
+			        .style("font-family", "sans-serif") //font styling.. 
   				    .text("Anscombe II");	
 
 	// Now, we will start actually building our scatterplot!
